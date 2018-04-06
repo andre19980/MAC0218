c8caa372ee7bfb6f4004fd4e46481b20e88b276b
Rukasu <lucas.yau@usp.br>
2018-04-06 08:27:10 -0300

    Created "edit" article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+	def new
+		@article = Article.new
+	end
+
 	def index
 		@articles = Article.all
 	end
@@ -7,16 +11,30 @@ class ArticlesController < ApplicationController
 	def show
     	@article = Article.find(params[:id])
   	end
-	
-	def new
-	
-	end
 
 	def create
 		@article = Article.new(article_params)
 
-		@article.save
-		redirect_to @article
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
+
+	end
+
+	def edit
+  		@article = Article.find(params[:id])
+	end
+
+	def update
+	    @article = Article.find(params[:id])
+	 
+	    if @article.update(article_params)
+	      redirect_to @article
+	    else
+	      render 'edit'
+	    end
 	end
 
 	private
