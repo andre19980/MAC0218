b897404856c3972ac773efb5182f26e075aafa17
Victor Seiji Hariki <victorseijih@gmail.com>
2018-05-15 14:16:02 -0300

    Added contraints to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
+require 'digest'
+
 class User < ApplicationRecord
+    validates :name, presence: true
+    
+    validates :email, presence: true, format: {
+        with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
+        message: "is not a valid e-mail"
+    }
+
+    validates :pw_hash, presence: true, format: {
+        with: /[a-z0-9]{64}/
+    }
+
+    validates :pw_salt, presence: true, format: {
+        with: /[a-z0-9]{10}/i
+    }
 end
